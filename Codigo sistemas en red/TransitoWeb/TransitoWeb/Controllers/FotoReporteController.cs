7cc74792c74854a4e2ec4c7018432d06740e7e44
Juan Carlos Domínguez Domínguez <36394805+JuuanC@users.noreply.github.com>
2019-06-17 12:54:19 -0500

    Servicios fotos

--- Codigo sistemas en red/TransitoWeb/TransitoWeb/Controllers/FotoReporteController.cs
+++ Codigo sistemas en red/TransitoWeb/TransitoWeb/Controllers/FotoReporteController.cs
@@ -3,12 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TransitoWeb.Models;
 
 namespace TransitoWeb.Controllers
 {
     public class FotoReporteController : Controller
     {
-        
+        [HttpGet]
+        public String Registro(String imagen, int idReporte)
+        {
+            using (TransitoContext dbSS = new TransitoContext())
+            {
+                byte[] archivo = System.Convert.FromBase64String(imagen);
+                FotoReporte foto = new FotoReporte();
+                foto.IdReporte = idReporte;
+                foto.Imagen = archivo;
+                dbSS.FotoReporte.Add(foto);
+                if (dbSS.SaveChanges() == 1)
+                {
+                    return "{\"si\"}";
+                }
+                else
+                    return "{\"no\"}";
+            }
+        }
+
+        [HttpPost]
+        public List<FotoReporte> listaFotos(int idReporte)
+        {
+            List<FotoReporte> fotos = null;
+            using (TransitoContext dbSS = new TransitoContext())
+            {
+                fotos = dbSS.FotoReporte
+                         .Where(a => a.IdReporte == idReporte)
+                         .ToList();
+            }
+            return fotos;
+        }
 
     }
 }
